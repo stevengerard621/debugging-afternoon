0219df9aad1efba29562d4e89ba47e0794209c15
Steven Gerard <stevengerard@Stevens-MacBook-Pro.local>
2019-12-17 15:52:14 -0700

    black diamond finished

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -12,6 +12,10 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.addToCart = this.addToCart.bind(this)
+    this.navigate = this.navigate.bind(this)
+    this.removeFromCart = this.removeFromCart.bind(this)
+
   }
   componentDidMount() {
     axios
@@ -28,6 +32,7 @@ class App extends Component {
     });
   }
   removeFromCart(index) {
+    console.log(this.removeFromCart)
     let cartCopy = this.state.cart.slice();
     cartCopy.splice(index, 1);
     this.setState({
@@ -36,9 +41,13 @@ class App extends Component {
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState({
+        showCart: true
+      });
     } else {
-      this.state.showCart = false;
+      this.setState({
+        showCart: false
+      });
     }
   }
   render() {
